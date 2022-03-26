4ddc102b7fc75466cef67f318c172f8d9bd6b4b7
Priybhash Soni    प्रियभाष सोनी <py.priybhashsoni@gmail.com>
2022-03-26 21:31:49 +1100

    Create Merge_Two_SortedList.java

---
+++ LeetCode/Merge_Two_SortedList.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode temp;
+        ListNode *x =&temp;
+        temp.next=NULL;
+        ListNode* t1=list1;
+        ListNode* t2=list2;
+        while(t1!=NULL && t2!=NULL)
+        {
+            if(t1->val<=t2->val)
+            {
+                x->next=t1;
+                t1=t1->next;
+                x=x->next;
+            }
+            else{
+                x->next=t2;
+                t2=t2->next;
+                x=x->next;
+            }
+        }
+        while(t1!=NULL)
+        {
+            x->next=t1;
+            t1=t1->next;
+            x=x->next;
+        }
+        while(t2!=NULL)
+        {
+            x->next=t2;
+            t2=t2->next;
+            x=x->next;
+        }
+        return temp.next;
+    }
+};
